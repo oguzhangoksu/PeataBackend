11e4fdbd755fd09c074a8d8fc2386145360f7a00
Oğuzhan Göksu <oguzhan.goksu@std.antalya.edu.tr>
2024-10-06 17:37:40 +0300

    fixed UserRepository

--- src/main/java/peata/backend/repositories/UserRepository.java
+++ src/main/java/peata/backend/repositories/UserRepository.java
@@ -13,6 +13,6 @@ public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
     
-    @Query("SELECT u.email FROM User u WHERE u.city = :city AND u.district = :district AND u.isAllowedNotification= true AND u.email <> :publisherEmail")
+    @Query("SELECT u.email FROM User u WHERE u.city = :city AND u.district = :district AND u.isAllowedNotification= true AND u.email <> :publisherEmail AND u.role='ROLE_USER' ")
     List<String> findEmailsByCityAndDistrict(String city, String district,String publisherEmail);
 } 
