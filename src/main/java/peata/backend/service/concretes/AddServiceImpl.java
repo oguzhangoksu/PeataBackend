30c88bfa88a822a03a40d6624d274fc5036284e2
Oğuzhan Göksu <oguzhan.goksu@std.antalya.edu.tr>
2024-11-14 21:25:39 +0300

    fixed bug

--- src/main/java/peata/backend/service/concretes/AddServiceImpl.java
+++ src/main/java/peata/backend/service/concretes/AddServiceImpl.java
@@ -229,6 +229,7 @@ public class AddServiceImpl implements AddService{
         dto.setEmail(add.getEmail());
         dto.setStatus(add.getStatus());
         dto.setUser_id(add.getUser().getId());
+        dto.setPCode(add.getPcode());
         return dto;
     }
 
